673166bd5705be324c66b9ea5dc74dd99e708f48
Hugo Manzano <hugokolbe6@outlook.com>
2024-07-07 23:02:47 -0700

    Initial Commit

---
+++ Tochino/TochinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TochinoApp.swift
+//  Tochino
+//
+//  Created by Hugo Manzano on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TochinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
